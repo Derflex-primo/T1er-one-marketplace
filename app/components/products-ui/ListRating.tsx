481ab355f8f8c4dfdb114968c9ee5a73587f36e6
Derflex-primo <dryldeogracias@gmail.com>
2023-10-26 11:44:55 +0800

    initialized git bugg fixed #21

--- app/components/products-ui/ListRating.tsx
+++ app/components/products-ui/ListRating.tsx
@@ -24,30 +24,39 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
 
   return (
     <div className="max-w-[650px]">
-     
       <div className="mt-8 max-h-[400px] overflow-y-auto scrollbar-thin scrollbar-thumb-black scrollbar-track-transparent">
         {product.reviews === undefined ? (
           <h1>No reviews</h1>
         ) : (
           product.reviews.map((review: any, index: number) => {
             const expanded = expandedStates[index];
-            const hasLongComment = review.comment.length > 180;
+            const hasLongComment =
+              review.comment &&
+              typeof review.comment === "string" &&
+              review.comment.length > 180;
 
             return (
               <div key={review.id} className="max-w-[95%]">
                 <div className="flex gap-2 items-center">
-                  <Avatar src={review?.user.image} />
-                  <div className="">{review?.user.name}</div>
+                  <Avatar src={review?.user?.image || null} />
+                  <div className="">{review?.user?.name || null}</div>
                   <div className="font-thin text-xs text-stone-400">
                     {moment(review.createdData).fromNow()}
                   </div>
                 </div>
 
                 <div className="mt-2">
-                  <Rating value={review.rating} sx={{ fontSize: "1rem" }} />
+                  <Rating
+                    value={review?.rating || 0}
+                    sx={{ fontSize: "1rem" }}
+                  />
 
                   <div className="ml-2 text-stone-500 text-xs text-justify gap-4">
-                    {expanded ? review.comment : review.comment.slice(0, 180)}
+                    {expanded
+                      ? review.comment
+                      : review.comment && typeof review.comment === "string"
+                      ? review.comment.slice(0, 180)
+                      : ""}
                     {!expanded && hasLongComment && "..."}
                     {hasLongComment && (
                       <button
