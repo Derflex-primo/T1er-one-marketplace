2686add709b157a5e9494d90324997bde892cae1
Derflex-primo <dryldeogracias@gmail.com>
2023-11-07 21:23:58 +0800

    initialized git log out p-3  fixed #34

--- app/components/user-infos-ui/Profile.tsx
+++ app/components/user-infos-ui/Profile.tsx
@@ -89,7 +89,7 @@ const Profile: React.FC<Profile> = ({ scrolled, connectWallet }) => {
                 href={`/`}>
               <div
                 onClick={user ? () => handleSignOut() : undefined}
-                className={`p-2 select-none text-stone-900 font-semibold rounded-xl flex items-center space-x-4 hover:bg-stone-100   trasition ease-in-out duration-150 ${
+                className={`p-3 select-none text-stone-900 font-semibold rounded-xl flex items-center space-x-4 hover:bg-stone-100   trasition ease-in-out duration-150 ${
                   !user ? "cursor-not-allowed" : "cursor-pointer"
                 }`}
               >
